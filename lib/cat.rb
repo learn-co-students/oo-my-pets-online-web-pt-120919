07ee4b2a57d5d88df531c617e89e5df21ca853df
Carlos Barreiro <bcarlosro@gmail.com>
2020-01-03 13:14:56 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
-class Cat
-  # code goes here
+@@all = []
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def self.all
+    @@all
+  end
+
+  def feed 
+    @mood = "happy"
+  end
+
+  def sell 
+    @mood = "nervous"
+    @owner = nil 
+  end
+
 end
