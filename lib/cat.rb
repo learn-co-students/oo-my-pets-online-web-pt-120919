33affcb803e17b0a8f40e6bbc834e26d61ba9fd6
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 03:57:53 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+  
+  @@all = []
+  
+  attr_accessor :owner,:mood
+  attr_reader :name
+  
+  def initialize(name,owner_obj)
+    @name = name
+    @owner = owner_obj
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
