6eebdf67c49add9f0f6b3f099b5cda5b590c5881
Nguyen Ha <nynha0526@gmail.com>
2020-01-21 00:26:48 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,17 @@
 class Cat
-  def cat 
-    attr_accessor 
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
